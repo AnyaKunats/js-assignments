21b32ee23ab9cbd00e910e880de7859b483c18f4
AnyaKunats <36382350+AnyaKunats@users.noreply.github.com>
2019-04-11 23:16:08 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -53,7 +53,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    return value1/2+value2/2;
+  return value1/2+value2/2;
 }
 
 /**
@@ -112,7 +112,9 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.acos((x1*x2+y1*y2)/(Math.sqrt(x1*x1+y1*y1)*Math.sqrt(x2*x2+y2*y2)));
+  const a=Math.sqrt(x1*x1+y1*y1);
+  const b=Math.sqrt(x2*x2+y2*y2);
+  return Math.acos((x1*x2+y1*y2)/(a*b));
 }
 
 /**
@@ -182,7 +184,7 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-  return Math.round(num/Math.pow(10,pow))*Math.pow(10,pow);
+  return Math.round(num/Math.pow(10, pow))*Math.pow(10, pow);
 }
 
 /**
