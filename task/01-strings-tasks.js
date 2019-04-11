da8f41c6208ba0ae0ef960f3e3887d579186e0ef
AnyaKunats <36382350+AnyaKunats@users.noreply.github.com>
2019-04-11 23:10:09 +0300

    Tasks

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -277,10 +277,10 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-  const let s1='A♣,2♣,3♣,4♣,5♣,6♣,7♣,8♣,9♣,10♣,J♣,Q♣,K♣,A♦,2♦,3♦,4♦';
-  const let s2='5♦,6♦,7♦,8♦,9♦,10♦,J♦,Q♦,K♦,A♥,2♥,3♥,4♥,5♥,6♥,7♥,8♥';
-  const let s3='9♥,10♥,J♥,Q♥,K♥,A♠,2♠,3♠,4♠,5♠,6♠,7♠,8♠,9♠,10♠,J♠,Q♠,K♠';
-  const let str=s1+s2+s3;
+  const s1='A♣,2♣,3♣,4♣,5♣,6♣,7♣,8♣,9♣,10♣,J♣,Q♣,K♣,A♦,2♦,3♦,4♦';
+  const s2='5♦,6♦,7♦,8♦,9♦,10♦,J♦,Q♦,K♦,A♥,2♥,3♥,4♥,5♥,6♥,7♥,8♥';
+  const s3='9♥,10♥,J♥,Q♥,K♥,A♠,2♠,3♠,4♠,5♠,6♠,7♠,8♠,9♠,10♠,J♠,Q♠,K♠';
+  const str=s1+s2+s3;
   return str.split(',').indexOf(value);
 }
 
